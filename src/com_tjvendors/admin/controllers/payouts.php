a44626e666a61555d927db86cba8ddec7cb02f23
“Mayur <mayur_s@techjoomla.com>
2017-01-25 20:01:15 +0530

    #94950 Payouts And Reports( Removal of unnecessary code and filter working fine)

--- src/com_tjvendors/admin/controllers/payouts.php
+++ src/com_tjvendors/admin/controllers/payouts.php
@@ -22,39 +22,6 @@ use Joomla\Utilities\ArrayHelper;
 class TjvendorsControllerPayouts extends JControllerAdmin
 {
 	/**
-	 * Method to clone existing Vendors
-	 *
-	 * @return void
-	 */
-	public function duplicate()
-	{
-		// Check for request forgeries
-		Jsession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
-
-		// Get id(s)
-		$pks = $this->input->post->get('cid', array(), 'array');
-
-		try
-		{
-			if (empty($pks))
-			{
-				throw new Exception(JText::_('COM_TJVENDORS_NO_ELEMENT_SELECTED'));
-			}
-
-			ArrayHelper::toInteger($pks);
-			$model = $this->getModel();
-			$model->duplicate($pks);
-			$this->setMessage(Jtext::_('COM_TJVENDORS_ITEMS_SUCCESS_DUPLICATED'));
-		}
-		catch (Exception $e)
-		{
-			JFactory::getApplication()->enqueueMessage($e->getMessage(), 'warning');
-		}
-
-		$this->setRedirect('index.php?option=com_tjvendors&view=payouts');
-	}
-
-	/**
 	 * Proxy for getModel.
 	 *
 	 * @param   string  $name    Optional. Model name
@@ -70,59 +37,5 @@ class TjvendorsControllerPayouts extends JControllerAdmin
 		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
 
 		return $model;
-	}
-
-	/**
-	 * Redirect.
-	 *
-	 * @return void
-	 * 
-	 * @since    1.6
-	 */
-	public function getRedirectToList()
-	{
-		// Print_r($_POST);die;
-
-		// Get the input
-		$input = JFactory::getApplication()->input;
-		$vendor_id   = $input->post->get('vendor_id', '', 'INT');
-
-		// $append = parent::getRedirectToListAppend();
-
-		$append = 'index.php?option=com_tjvendors&view=payouts&client=com_jticketing&vendor_id=' . $vendor_id;
-		$this->setRedirect($append);
-	}
-
-	/**
-	 * Method to save the submitted ordering values for records via AJAX.
-	 *
-	 * @return  void
-	 *
-	 * @since   3.0
-	 */
-	public function saveOrderAjax()
-	{
-		// Get the input
-		$input = JFactory::getApplication()->input;
-		$pks   = $input->post->get('cid', array(), 'array');
-		$order = $input->post->get('order', array(), 'array');
-
-		// Sanitize the input
-		ArrayHelper::toInteger($pks);
-		ArrayHelper::toInteger($order);
-
-		// Get the model
-		$model = $this->getModel();
-
-		// Save the ordering
-		$return = $model->saveorder($pks, $order);
-
-		if ($return)
-		{
-			echo "1";
-		}
-
-		// Close the application
-		JFactory::getApplication()->close();
 	}
 }

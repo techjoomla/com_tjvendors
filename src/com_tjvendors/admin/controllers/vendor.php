cc169718f8dab5d81d2a916934b580f167ce922e
“Mayur <mayur_s@techjoomla.com>
2017-07-05 11:51:02 +0530

    bug fixes and publish unpublish fixed

--- src/com_tjvendors/admin/controllers/vendor.php
+++ src/com_tjvendors/admin/controllers/vendor.php
@@ -104,4 +104,116 @@ class TjvendorsControllerVendor extends JControllerForm
 
 		return $append;
 	}
+
+	/**
+	 * Save vendor data
+	 *
+	 * @param   integer  $key     key.
+	 *
+	 * @param   integer  $urlVar  url
+	 *
+	 * @return  string  The arguments to append to the redirect URL.
+	 *
+	 * @since   1.6
+	 */
+	public function save($key = null, $urlVar = null)
+	{
+		// Initialise variables.
+		$app   = JFactory::getApplication();
+		$model = $this->getModel('Vendor', 'TjvendorsModel');
+		$input = $app->input;
+		$client = $input->get('client', '', 'STRING');
+
+		// Get the user data.
+		$data = JFactory::getApplication()->input->get('jform', array(), 'array');
+
+		// Validate the posted data.
+		$form = $model->getForm();
+
+		if (!$form)
+		{
+			JError::raiseError(500, $model->getError());
+
+			return false;
+		}
+
+		// Validate the posted data.
+		$data = $model->validate($form, $data);
+
+		// Check for errors.
+		if ($data === false)
+		{
+			// Get the validation messages.
+			$errors = $model->getErrors();
+
+			// Push up to three validation messages out to the user.
+			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++)
+			{
+				if ($errors[$i] instanceof Exception)
+				{
+					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
+				}
+				else
+				{
+					$app->enqueueMessage($errors[$i], 'warning');
+				}
+			}
+			// Redirect back to the edit screen.
+			$id = (int) $app->getUserState('com_tjvendors.edit.vendor.id');
+			$this->setRedirect(JRoute::_('index.php?option=com_tjvendors&view=vendor&layout=edit&client=' . $client . '&id=' . $id, false));
+
+			return false;
+		}
+
+		$return = $model->save($data);
+
+		// Check for errors.
+		if ($return === false)
+		{
+			// Redirect back to the edit screen.
+			$id = (int) $app->getUserState('com_tjvendors.edit.vendor.id');
+			$this->setMessage(JText::sprintf('COM_TJVENDORS_VENDOR_ERROR_MSG_SAVE', $model->getError()), 'warning');
+			$this->setRedirect(JRoute::_('index.php?option=com_tjvendors&view=vendor&layout=edit&client=' . $client . '&id=' . $id, false));
+
+			return false;
+		}
+
+		$msg      = JText::_('COM_TJVENDORS_MSG_SUCCESS_SAVE_VENDOR');
+		$id = $input->get('id');
+
+		if (empty($id))
+		{
+			$id = $return;
+		}
+
+		$task = $input->get('task');
+
+		if ($task == 'apply')
+		{
+			$redirect = JRoute::_('index.php?option=com_tjvendors&view=vendor&layout=edit&client=' . $client . '&id=' . $id, false);
+			$app->redirect($redirect, $msg);
+		}
+
+		if ($task == 'save2new')
+		{
+			$redirect = JRoute::_('index.php?option=com_tjvendors&view=vendor&layout=edit&client=' . $client, false);
+			$app->redirect($redirect, $msg);
+		}
+
+		// Clear the profile id from the session.
+		$app->setUserState('com_tjvendors.edit.vendor.id', null);
+
+		// Check in the profile.
+		if ($return)
+		{
+			$model->checkin($return);
+		}
+
+		// Redirect to the list screen.
+		$redirect = JRoute::_('index.php?option=com_tjvendors&view=vendors&client=' . $client, false);
+		$app->redirect($redirect, $msg);
+
+		// Flush the data from the session.
+		$app->setUserState('com_tjvendors.edit.vendor.data', null);
+	}
 }

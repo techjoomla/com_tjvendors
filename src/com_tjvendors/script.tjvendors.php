d20d4c9b44f914992a73dbe18cf291b834b65d1e
“Mayur <mayur_s@techjoomla.com>
2018-01-20 11:08:06 +0530

    Issue #116426 feat: Merging Security fixes changes on multivendor(administrator)

--- src/com_tjvendors/script.tjvendors.php
+++ src/com_tjvendors/script.tjvendors.php
@@ -48,7 +48,7 @@ class Com_TjvendorsInstallerScript
 	 *
 	 * @return void
 	 */
-	public function postflight($type, $parent)
+	public function postflight( $type, $parent )
 	{
 		// Write template file for email template
 		$this->_insertTjNotificationTemplates();

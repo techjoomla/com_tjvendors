12838a0f9915ac545c6a3bfb5e328e1945fc36a4
Snehal Patil <snehal_patil@techjoomla.com>
2018-08-20 14:29:02 +0530

    Bug #132519 fix: Vendor approval - If the config is off still vendor is unpublished.

--- src/com_tjvendors/admin/views/vendor/tmpl/edit.php
+++ src/com_tjvendors/admin/views/vendor/tmpl/edit.php
@@ -33,7 +33,6 @@ tjVAdmin.vendor.initVendorJs();
 
 <form action="<?php echo JRoute::_('index.php?option=com_tjvendors&layout=edit&client='.$this->input->get('client', '', 'INTEGER').'&vendor_id=' . (int) $this->item->vendor_id); ?>"
 	method="post" enctype="multipart/form-data" name="adminForm" id="adminForm" class="form-validate">
-	<input type="hidden" name="jform[state]" value="<?php echo $this->item->state; ?>" />
 	<div class="form-horizontal">
 		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'personal')); ?>
 		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'personal', JText::_('COM_TJVENDORS_TITLE_PERSONAL', true)); ?>
@@ -75,6 +74,7 @@ tjVAdmin.vendor.initVendorJs();
 							echo $this->form->renderField('client');
 							echo $this->form->renderField('vendor_title');
 							echo $this->form->renderField('alias');
+							echo $this->form->renderField('state');
 							echo $this->form->renderField('vendor_description');
 							echo $this->form->renderField('vendor_logo');
 							if(empty($this->item->vendor_logo)) :?>

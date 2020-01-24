a3e9de9d7786facc08c7c782e921f6b91a608a7a
Nitesh Kesarkar <nitesh_k@techjoomla.com>
2020-01-24 13:02:41 +0530

    Task #156244 Chore: Add checkPaymentGateway method in vendor class

---
+++ src/com_tjvendors/site/includes/tjvendors.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * @package     TJVendors
+ * @subpackage  com_tjvendors
+ *
+ * @author      Techjoomla <extensions@techjoomla.com>
+ * @copyright   Copyright (C) 2009 - 2019 Techjoomla. All rights reserved.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use Joomla\CMS\Table\Table;
+use Joomla\String\StringHelper;
+
+/**
+ * TJVendors factory class.
+ *
+ * This class perform the helpful operation required to  TJVendors package
+ *
+ * @method  TjvendorsVersion  version()
+ * @method  TjvendorsVendor   vendor()
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class TJVendors
+{
+	/**
+	 * Holds the record of the loaded TJVendors classes
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	private static $loadedClass = array();
+
+	/**
+	 * Holds the record of the component config
+	 *
+	 * @var    Joomla\Registry\Registry
+	 * @since  __DEPLOY_VERSION__
+	 */
+	private static $config = null;
+
+	/**
+	 * Retrieves a table from the table folder
+	 *
+	 * @param   string  $name    The table file name
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  Table|boolean object or false on failure
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 **/
+	public static function table($name, $config = array())
+	{
+		Table::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_tjvendors/tables');
+		$table = Table::getInstance($name, 'TjvendorsTable', $config);
+
+		return $table;
+	}
+
+	/**
+	 * Retrieves a model from the model folder
+	 *
+	 * @param   string  $name    The model name
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  BaseDatabaseModel|boolean object or false on failure
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 **/
+	public static function model($name, $config = array())
+	{
+		BaseDatabaseModel::addIncludePath(JPATH_SITE . '/components/com_tjvendors/models', 'TJVendorsModel');
+		$model = BaseDatabaseModel::getInstance($name, 'TJVendorsModel', $config);
+
+		return $model;
+	}
+
+	/**
+	 * Magic method to create instance of Tjvendors library
+	 *
+	 * @param   string  $name       The name of the class
+	 * @param   mixed   $arguments  Arguments of class
+	 *
+	 * @return  mixed   return the Object of the respective class if exist OW return false
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 **/
+	public static function __callStatic($name, $arguments)
+	{
+		self::loadClass($name);
+
+		$className = 'Tjvendors' . StringHelper::ucfirst($name);
+
+		if (class_exists($className))
+		{
+			if (method_exists($className, 'getInstance'))
+			{
+				return call_user_func_array(array($className, 'getInstance'), $arguments);
+			}
+
+			return new $className;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Load the class library if not loaded
+	 *
+	 * @param   string  $className  The name of the class which required to load
+	 *
+	 * @return  boolean True on success
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 **/
+	public static function loadClass($className)
+	{
+		if (! isset(self::$loadedClass[$className]))
+		{
+			$className = (string) StringHelper::strtolower($className);
+
+			$path = JPATH_SITE . '/components/com_tjvendors/includes/' . $className . '.php';
+
+			include_once $path;
+
+			self::$loadedClass[$className] = true;
+		}
+
+		return self::$loadedClass[$className];
+	}
+
+	/**
+	 * Load the component configuration
+	 *
+	 * @return  Joomla\Registry\Registry  A Registry object.
+	 */
+	public static function config()
+	{
+		if (empty(self::$config))
+		{
+			self::$config = ComponentHelper::getParams('com_tjvendors');
+		}
+
+		return self::$config;
+	}
+
+	/**
+	 * Initializes the css, js and necessary dependencies
+	 *
+	 * @param   string  $location  The location where the assets needs to load
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public static function init($location = 'site')
+	{
+		static $loaded = null;
+		$docType = Factory::getDocument()->getType();
+		$versionClass = self::version();
+
+		if ($loaded[$location] && ($docType != 'html'))
+		{
+			return;
+		}
+
+		$version = $versionClass->getMediaVersion();
+		$options = array("version" => $version);
+
+		HTMLHelper::script('media/com_tjvendor/js/tjvendors.js', $options);
+		HTMLHelper::stylesheet('media/com_tjvendor/css/tjvendors.css', $options);
+		HTMLHelper::stylesheet('media/techjoomla_strapper/vendors/no-more-tables.css', $options);
+
+		// Load Boostrap
+		if (self::config()->get('load_bootstrap') == '1')
+		{
+			define('COM_TJVENDORS_WRAPPAER_CLASS', "tjBs3");
+			HTMLHelper::_('stylesheet', 'media/techjoomla_strapper/bs3/css/bootstrap.min.css');
+			HTMLHelper::_('stylesheet', 'media/techjoomla_strapper/vendors/font-awesome/css/font-awesome.min.css');
+		}
+		else
+		{
+			define('COM_TJVENDORS_WRAPPAER_CLASS', '');
+		}
+
+		$loaded[$location] = true;
+	}
+}

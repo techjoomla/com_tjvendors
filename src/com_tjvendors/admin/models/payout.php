8f60747ea0ed19fac218ba77dc42982901f48f38
“aishwarya <aishwarya_b@techjoomla.com>
2020-05-18 16:54:14 +0530

    Bug#157642 fix:Showing 'Vendor fee successfully saved' message on after payouts flow completion

--- src/com_tjvendors/admin/models/payout.php
+++ src/com_tjvendors/admin/models/payout.php
@@ -196,10 +196,6 @@ class TjvendorsModelPayout extends AdminModel
 						// Update their details in the users table using id as the primary key.
 						Factory::getDbo()->updateObject('#__tjvendors_passbook', $payout_update, 'id');
 					}
-
-
-					$message = Text::_('COM_TJVENDORS_PAYOUT_SUCCESSFULL_MESSAGE');
-					Factory::getApplication()->enqueueMessage($message);
 				}
 
 				return true;

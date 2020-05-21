64d9cf4a52aa252e07969c7ef98e06fc7626d803
“Deepa <deepgawade89@gmail.com>
2020-05-21 16:11:35 +0530

    Bug #160919 fix: Campaigns Promoter Report - After clear fields Region, City the value appearing in report

--- src/com_tjvendors/media/js/ui/common.js
+++ src/com_tjvendors/media/js/ui/common.js
@@ -15,7 +15,16 @@ export class CommonUI{
 	{
 		var countryId = jQuery("#" + country).val();
 
-		if (typeof countryId === "undefined") {
+		if (countryId == 0) {
+			jQuery("#jform_region").val(0);
+			jQuery("#jform_city").val(0);
+			
+			let regionField = document.getElementById('jform_region');
+			jQuery(regionField).trigger("liszt:updated");
+
+			let cityField = document.getElementById('jform_city');
+			jQuery(cityField).trigger("liszt:updated");
+			
 			return;
 		}
 

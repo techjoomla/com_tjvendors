f5129a21bdbfb3315492cae31225cd2f80917f15
Ankush Maherwal <ankush_m@techjoomla.com>
2021-03-05 17:05:25 +0530

    Bug #169366 fix: Housekeeping script of version 1.4.2 gives error for site where TJ-Notifications is not installed

--- src/com_tjvendors/admin/houseKeeping/1.4.2/emailTemplate.php
+++ src/com_tjvendors/admin/houseKeeping/1.4.2/emailTemplate.php
@@ -13,6 +13,7 @@ defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Table\Table;
+use Joomla\CMS\Component\ComponentHelper;
 
 /**
  * TJvendors email notification migrations
@@ -38,54 +39,60 @@ class TjHouseKeepingEmailTemplate extends TjModelHouseKeeping
 
 		try
 		{
-			Table::addIncludePath(JPATH_ROOT . '/administrator/components/com_tjnotifications/tables');
+			// Get com_subusers component status
+			$tjNotificationsInstalled = ComponentHelper::getComponent('com_tjnotifications', true)->enabled;
 
-			$db    = Factory::getDbo();
-			$query = $db->getQuery(true);
-			$query->select("temp.id as tempId");
-			$query->select("conf.id as confId");
-			$query->select("conf.subject");
-			$query->select("conf.body");
-			$query->from($db->qn('#__tj_notification_templates', 'temp'));
-			$query->join('LEFT', $db->qn('#__tj_notification_template_configs', 'conf') .
-			' ON (' . $db->qn('temp.id') . ' = ' . $db->qn('conf.template_id') . ')');
-			$query->where($db->qn('temp.client') . ' = ' . $db->quote("com_tjvendors"));
-			$query->where($db->qn('conf.backend') . ' = ' . $db->quote("email"));
-			$db->setQuery($query);
-			$emailArray = $db->loadAssocList();
-
-			if (!empty($emailArray))
+			if ($tjNotificationsInstalled)
 			{
-				foreach ($emailArray as $key => $value)
+				Table::addIncludePath(JPATH_ROOT . '/administrator/components/com_tjnotifications/tables');
+
+				$db    = Factory::getDbo();
+				$query = $db->getQuery(true);
+				$query->select("temp.id as tempId");
+				$query->select("conf.id as confId");
+				$query->select("conf.subject");
+				$query->select("conf.body");
+				$query->from($db->qn('#__tj_notification_templates', 'temp'));
+				$query->join('LEFT', $db->qn('#__tj_notification_template_configs', 'conf') .
+				' ON (' . $db->qn('temp.id') . ' = ' . $db->qn('conf.template_id') . ')');
+				$query->where($db->qn('temp.client') . ' = ' . $db->quote("com_tjvendors"));
+				$query->where($db->qn('conf.backend') . ' = ' . $db->quote("email"));
+				$db->setQuery($query);
+				$emailArray = $db->loadAssocList();
+
+				if (!empty($emailArray))
 				{
-					if ((!empty($value['subject'])) || (!empty($value['body'])))
+					foreach ($emailArray as $key => $value)
 					{
-						$table   = Table::getInstance('Template', 'TjnotificationTable', array());
-						$obj     = new stdClass;
-						$obj->id = $value['confId'];
-
-						if (strpos($value['subject'], '{{') === false)
+						if ((!empty($value['subject'])) || (!empty($value['body'])))
 						{
-							$subjectBracketReplace  = str_replace("{", "{{", $value['subject']);
-							$subjectBracketReplace  = str_replace("}", "}}", $subjectBracketReplace);
-							$obj->subject           = $subjectBracketReplace;
-						}
+							$table   = Table::getInstance('Template', 'TjnotificationTable', array());
+							$obj     = new stdClass;
+							$obj->id = $value['confId'];
 
-						if (strpos($value['body'], '{{') === false)
-						{
-							$bodyBracketReplace  = str_replace("{", "{{", $value['body']);
-							$bodyBracketReplace  = str_replace("}", "}}", $bodyBracketReplace);
-							$obj->body           = $bodyBracketReplace;
-						}
+							if (strpos($value['subject'], '{{') === false)
+							{
+								$subjectBracketReplace  = str_replace("{", "{{", $value['subject']);
+								$subjectBracketReplace  = str_replace("}", "}}", $subjectBracketReplace);
+								$obj->subject           = $subjectBracketReplace;
+							}
 
-						$table->save($obj);
+							if (strpos($value['body'], '{{') === false)
+							{
+								$bodyBracketReplace  = str_replace("{", "{{", $value['body']);
+								$bodyBracketReplace  = str_replace("}", "}}", $bodyBracketReplace);
+								$obj->body           = $bodyBracketReplace;
+							}
+
+							$table->save($obj);
+						}
 					}
+
+					$result['status']  = true;
+					$result['message'] = "Migration is done successfully";
+
+					return $result;
 				}
-
-				$result['status']  = true;
-				$result['message'] = "Migration is done successfully";
-
-				return $result;
 			}
 		}
 		catch (Exception $e)

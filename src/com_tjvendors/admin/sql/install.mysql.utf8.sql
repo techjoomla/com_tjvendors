0aabdd790e1ba1b00e13fecf5f375acdf7bf6b27
Ankush Maherwal <ankush_m@techjoomla.com>
2021-06-14 22:09:53 +0530

    Task #172430 feat: Update JGive code to make Joomla 4 compatible

--- src/com_tjvendors/admin/sql/install.mysql.utf8.sql
+++ src/com_tjvendors/admin/sql/install.mysql.utf8.sql
@@ -61,9 +61,3 @@ CREATE TABLE IF NOT EXISTS `#__tjvendors_vendors` (
   `modified_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`vendor_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO `#__content_types` (`type_title`, `type_alias`, `table`, `content_history_options`, `rules`, `field_mappings`)
-SELECT * FROM ( SELECT 'Vendor','com_tjvendors.vendor','{"special":{"dbtable":"#__tjvendors_vendors","key":"id","type":"Vendor","prefix":"TjTable"}}', '{"formFile":"administrator\/components\/com_tjvendors\/models\/forms\/vendor.xml", "hideFields":["checked_out","checked_out_time","params","language"], "ignoreChanges":["modified_by", "modified", "checked_out", "checked_out_time"], "convertToInt":["publish_up", "publish_down"], "displayLookup":[{"sourceColumn":"catid","targetTable":"#__categories","targetColumn":"id","displayColumn":"title"},{"sourceColumn":"group_id","targetTable":"#__usergroups","targetColumn":"id","displayColumn":"title"},{"sourceColumn":"created_by","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},{"sourceColumn":"access","targetTable":"#__viewlevels","targetColumn":"id","displayColumn":"title"},{"sourceColumn":"modified_by","targetTable":"#__users","targetColumn":"id","displayColumn":"name"}]}') AS tmp
-WHERE NOT EXISTS (
-	SELECT type_alias FROM `#__content_types` WHERE (`type_alias` = 'com_tjvendors.vendor')
-) LIMIT 1;
